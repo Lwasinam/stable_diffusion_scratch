88d09e643a1932cde6a0cd157f30614949165e8c
Lwasinam <lwasinamdilli@gmail.com>
2024-05-11 12:21:21 +0100

    e bug

--- sd/dataset.py
+++ sd/dataset.py
@@ -33,7 +33,7 @@ class ImageDataset(Dataset):
                  # convert the prompt into tokens using the tokenizer
         cond_tokens = self.tokenizer([text], padding = 'max_length', max_length = 77).input_ids
             # (batch_size, seq_len)   
-        # cond_tokens = torch.tensor(cond_tokens, dtype= torch.long, device = device)
+        cond_tokens = torch.tensor(cond_tokens, dtype= torch.long, device = device)
         cond_tokens = cond_tokens.squeeze(0)
         return {
             'image_tensor': input_image_tensor,
